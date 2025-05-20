190355f683d2336b69c28c4025b2916f1ee096ef
XiaofengdiZhu <523084467@qq.com>
2025-05-20 21:12:54 +0800

    添加替换方块例子、更新readme

---
+++ SurvivalcraftTemplateModForAPI/TemplateReplaceBlock.cs
@@ -0,0 +1,9 @@
+﻿using Game;
+
+namespace YourModNamespace {
+    public class DirtBlock : Game.DirtBlock {
+        public new static int Index = 2;
+        public override string GetDisplayName(SubsystemTerrain subsystemTerrain, int value) => "Replaced Dirt";
+        public override string GetDescription(int value) => "This Dirt Block has been replaced";
+    }
+}
