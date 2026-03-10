c0027d0eeded908b62a21cb55631ebf31d583cfe
XiaofengdiZhu <523084467@qq.com>
2026-03-10 20:46:03 +0800

    补充部件教程、AGENTS.md

--- SurvivalcraftTemplateModForAPI/TemplateWidget.cs
+++ SurvivalcraftTemplateModForAPI/TemplateWidget.cs
@@ -5,9 +5,17 @@ namespace Game {
     // 该部件是在 ComponentTemplate 中创建并添加到界面中的
     // This widget is instantiated in ComponentTemplate and added to the GUI.
     public class TemplateWidget : CanvasWidget {
+        Random m_random = new Random();
+        LabelWidget m_label;
         public TemplateWidget() {
             LoadContents(this, ContentManager.Get<XElement>("Widgets/TemplateWidget"));
+            m_label = Children.Find<LabelWidget>("TemplateLabel");
             Log.Information("Survivalcraft Template Mod: Template Widget Instantiated. 来自生存战争示例模组：示例部件已实例化。");
+        }
+
+        public override void Update() {
+            int color = m_random.Int();
+            m_label.Color = new Color(color & 0xFF, (color >> 8) & 0xFF, (color >> 16) & 0xFF);
         }
     }
 }
