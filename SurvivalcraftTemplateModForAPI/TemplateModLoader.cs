80a9e325ec8b68fa5d7632b36bed1fdf008f3690
XiaofengdiZhu <523084467@qq.com>
2026-07-16 02:23:26 +0800

    适配 API 1.9.3

--- SurvivalcraftTemplateModForAPI/TemplateModLoader.cs
+++ SurvivalcraftTemplateModForAPI/TemplateModLoader.cs
@@ -5,19 +5,21 @@ using HarmonyLib;
 
 namespace Game {
     public class TemplateModLoader : ModLoader {
+        public const string PackageName = "xfdz.SurvivalcraftTemplateMod";
         public override void __ModInitialize() {
             ModsManager.RegisterHook("OnLoadingFinished", this);
 
             // 你可以在此处进行 Harmony 的 Patch
             // You can do Harmony Patch at here.
-            Harmony harmony = new Harmony("xfdz.SurvivalcraftTemplateMod");
+            Harmony harmony = new Harmony(PackageName);
             harmony.PatchAll();
         }
 
         // 必须在 __ModInitialize() 方法中注册，否则无效
         // You must register it at __ModInitialize(), otherwise it will not work.
         public override void OnLoadingFinished(List<Action> actions) {
-            Log.Information("Survivalcraft Template Mod: Game Loaded. 来自生存战争示例模组：游戏已加载。");
+            ModSettingsManager.TryGet(out bool setting, PackageName, "TemplateModSettingsGroup1", "TemplateModSettingsItem1");
+            Log.Information($"Survivalcraft Template Mod: Game Loaded. A setting: {setting} 来自生存战争示例模组：游戏已加载。一个设置：{setting}");
         }
     }
 }
